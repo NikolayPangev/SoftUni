68a0bf36c73b21057473bcc58c53349c65faf23c
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-12-26 20:29:16 +0200

    Conditional Statements Advanced - Lab

---
+++ Java/Programming Basics/Conditional Statements Advanced/Conditional Statements Advanced - Lab/_10_InvalidNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class _10_InvalidNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if ((number >= 100 && number <= 200) || number == 0){
+            return;
+        } else {
+            System.out.println("invalid");
+        }
+    }
+}
