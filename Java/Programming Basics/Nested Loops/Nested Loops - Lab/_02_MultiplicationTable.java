b6884edbb0b6ae5b3548a7a6b94dc09ea5493692
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-01-05 20:13:43 +0200

    Nested Loops - Lab

---
+++ Java/Programming Basics/Nested Loops/Nested Loops - Lab/_02_MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class _02_MultiplicationTable {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 10; y++) {
+                System.out.printf("%d * %d = %d\n", x, y, x * y);
+            }
+        }
+    }
+}
