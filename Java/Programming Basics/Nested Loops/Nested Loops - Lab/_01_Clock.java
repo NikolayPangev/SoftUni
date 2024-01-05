53f16642053f4b0ad6c594985697dcd024c8e819
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-01-05 20:14:50 +0200

    Nested Loops - Lab

--- Java/Programming Basics/Nested Loops/Nested Loops - Lab/_01_Clock.java
+++ Java/Programming Basics/Nested Loops/Nested Loops - Lab/_01_Clock.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 public class _01_Clock {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         for (int h = 0; h < 24; h++) {
             for (int m = 0; m < 60; m++) {
                 System.out.printf("%d:%d\n", h, m);
