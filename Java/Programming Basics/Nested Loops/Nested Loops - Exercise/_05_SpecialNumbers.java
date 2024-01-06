8a189a6a1a740594c254a1669f6c70c494f5282e
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-01-06 17:02:10 +0200

    Nested Loops - Exercise

---
+++ Java/Programming Basics/Nested Loops/Nested Loops - Exercise/_05_SpecialNumbers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class _05_SpecialNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myNumber = Integer.parseInt(scanner.nextLine());
+
+        for (int currentNumber = 1111; currentNumber <= 9999; currentNumber++) {
+
+            String textNumber = String.valueOf(currentNumber);
+            boolean isSpecial = true;
+
+            for (int position = 0; position < textNumber.length(); position++) {
+                int currentDigit = Integer.parseInt(textNumber.charAt(position) + "");
+                if (currentDigit == 0){
+                    isSpecial = false;
+                    break;
+                }
+                if (myNumber % currentDigit != 0) {
+                    isSpecial = false;
+                    break;
+                }
+            }
+            if (isSpecial) {
+                System.out.print(currentNumber + " ");
+            }
+        }
+    }
+}
