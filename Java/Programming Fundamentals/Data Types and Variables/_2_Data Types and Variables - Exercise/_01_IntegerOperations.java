1c0116af44463e81c1922697e2f2acd768d1859a
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-01-09 13:42:36 +0200

    _2_Data Types and Variables - Exercise

---
+++ Java/Programming Fundamentals/Data Types and Variables/_2_Data Types and Variables - Exercise/_01_IntegerOperations.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class _01_IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+        int num3 = Integer.parseInt(scanner.nextLine());
+        int num4 = Integer.parseInt(scanner.nextLine());
+
+        int sum = num1 + num2;
+        int division = sum / num3;
+        int product = division * num4;
+
+        System.out.println(product);
+    }
+}
