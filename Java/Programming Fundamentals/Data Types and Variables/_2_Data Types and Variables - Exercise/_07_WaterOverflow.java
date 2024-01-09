9f81d133615ab22dae48fae26cc5a9123e546894
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-01-09 14:04:32 +0200

    _2_Data Types and Variables - Exercise

---
+++ Java/Programming Fundamentals/Data Types and Variables/_2_Data Types and Variables - Exercise/_07_WaterOverflow.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class _07_WaterOverflow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int waterTank = 0;
+
+        for (int i = 0; i < number; i++) {
+            int quantities = Integer.parseInt(scanner.nextLine());
+
+            if (waterTank + quantities <= 255) {
+                waterTank += quantities;
+            } else {
+                System.out.println("Insufficient capacity!");
+            }
+        }
+        System.out.println(waterTank);
+    }
+}
