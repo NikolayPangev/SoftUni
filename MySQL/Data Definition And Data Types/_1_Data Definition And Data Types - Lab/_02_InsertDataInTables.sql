495725dbada1954460e82ba6d3a41bb67bbc0aae
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-01-10 21:15:46 +0200

    Data Definition And Data Types - Lab

---
+++ MySQL/Data Definition And Data Types/_1_Data Definition And Data Types - Lab/_02_InsertDataInTables.sql
@@ -0,0 +1,3 @@
+INSERT INTO `gamebar`.`employees` (`first_name`, `last_name`) VALUES ('Ivan', 'Petrov');
+INSERT INTO `gamebar`.`employees` (`first_name`, `last_name`) VALUES ('Dimitar', 'Georgiev');
+INSERT INTO `gamebar`.`employees` (`first_name`, `last_name`) VALUES ('Elena', 'Simeonova');
