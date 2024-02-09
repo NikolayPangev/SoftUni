c7331680981c9b9dd1d42c7962310026e0050d13
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-02-09 17:25:01 +0200

    Database Programmability - Exercise

--- MySQL/Database Programmability/_2_Database Programmability - Exercise/_16_EmailsTrigger.sql
+++ MySQL/Database Programmability/_2_Database Programmability - Exercise/_16_EmailsTrigger.sql
@@ -15,8 +15,9 @@ BEGIN
 	INSERT INTO notification_emails (recipient, subject, body)
     VALUES (NEW.account_id,
     CONCAT('Balance change for account: ', NEW.account_id), 
-    CONCAT('On ', DATE_FORMAT(NOW(), '%b %d %Y'), ' your balance was changed from ', NEW.old_sum, ' to ', NEW.new_sum, '.'));
-END; $
+    CONCAT('On ', DATE_FORMAT(NOW(), '%b %d %Y at %r'), ' your balance was changed from ', 
+    ROUND(NEW.old_sum, 0), ' to ', ROUND(NEW.new_sum, 0), '.'));
+END $
 
 DELIMITER ;
 
