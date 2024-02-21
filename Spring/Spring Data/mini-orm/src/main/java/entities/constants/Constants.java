26b5bb1d9091fa30c463ee496abe78cee9d47124
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-02-21 21:20:51 +0200

    Update Constants.java

--- Spring/Spring Data/mini-orm/src/main/java/entities/constants/Constants.java
+++ Spring/Spring Data/mini-orm/src/main/java/entities/constants/Constants.java
@@ -3,6 +3,7 @@ package entities.constants;
 public enum Constants {
     ;
     public static final String ID_COLUM_MISSING_MESSAGE = "Entity missing an Id column";
+    public static final String CLASS_MUST_BE_ENTITY_MESSAGE = "Class must be Entity";
     public static final String COMMA_SEPARATOR = ", ";
 
     public enum Queries {
