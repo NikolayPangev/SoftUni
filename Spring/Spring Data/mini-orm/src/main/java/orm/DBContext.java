293b8bc1eed8523a9e2bbe74e00c1e885ee3afe0
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-02-22 21:22:40 +0200

    Update DBContext.java

--- Spring/Spring Data/mini-orm/src/main/java/orm/DBContext.java
+++ Spring/Spring Data/mini-orm/src/main/java/orm/DBContext.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public interface DBContext<E> {
     boolean persist(E entity) throws SQLException, IllegalAccessException;
     Iterable<E> find (Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
-    Iterable<E> find (Class<E> table, String where);
+    Iterable<E> find (Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
 
     E findFirst(Class<E> table);
     E findFirst(Class<E> table, String where);
