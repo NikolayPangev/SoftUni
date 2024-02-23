81802627f4acd0d674b54e5b959238f6b8e7b4e7
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-02-23 21:03:31 +0200

    Update User.java

--- Spring/Spring Data/mini-orm/src/main/java/entities/User.java
+++ Spring/Spring Data/mini-orm/src/main/java/entities/User.java
@@ -1,44 +1,36 @@
 package entities;
 
 
+import annotations.Column;
+import annotations.Entity;
+import annotations.Id;
+import jdk.jshell.Snippet;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
+@Getter
+@Setter
+@Builder
+@Entity(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "username")
     private String username;
 
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "confirmPassword")
+    private String confirmPassword;
+
+    @Column(name = "age")
     private int age;
 
-    private LocalDate registration;
-
-    public User(String username, int age, LocalDate registration) {
-        this.username = username;
-        this.age = age;
-        this.registration = registration;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public LocalDate getRegistration() {
-        return registration;
-    }
-
-    public void setRegistration(LocalDate registration) {
-        this.registration = registration;
-    }
 }
