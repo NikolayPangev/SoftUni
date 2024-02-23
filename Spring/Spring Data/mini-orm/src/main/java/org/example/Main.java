e6c8c11b6121ad1ef073d125848d3d725d722aa3
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-02-23 21:03:37 +0200

    Update Main.java

--- Spring/Spring Data/mini-orm/src/main/java/org/example/Main.java
+++ Spring/Spring Data/mini-orm/src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
+import entities.Account;
+import entities.User;
+import orm.EntityManager;
+
+import java.sql.SQLException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws SQLException,
+            IllegalAccessException {
+        final EntityManager<User> userEntityManager = new EntityManager<>();
+        final EntityManager<Account> accountEntityManager = new EntityManager<>();
+
+        final User deleteTest = User.builder()
+                .id(1)
+                .username("Nikolay Pangev")
+                .age(22)
+                .build();
+
+        accountEntityManager.doCreate(Account.class);
+        accountEntityManager.doAlter(Account.class);
+        userEntityManager.doDelete(deleteTest);
+
+        System.out.println();
     }
 }
