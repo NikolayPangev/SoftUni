55ffa7c50b12508077f9104a4812ff1d39e39083
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-03-13 21:13:21 +0200

    Update Project

--- Spring/Spring Data/_03_IntroductionToHibernate/src/main/java/entities/Employee.java
+++ Spring/Spring Data/_03_IntroductionToHibernate/src/main/java/entities/Employee.java
@@ -128,4 +128,12 @@ public class Employee {
     public void setProjects(Set<Project> projects) {
         this.projects = projects;
     }
+
+    public void printFullNameDepNameAndSalary() {
+        System.out.println(String.format("%s %s from %s - $%s",
+                this.firstName,
+                this.lastName,
+                this.department.getName(),
+                this.salary));
+    }
 }
