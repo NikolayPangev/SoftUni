6d9f231cf9e18b6a378a86604510a2880e1b4817
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-03-24 22:13:57 +0200

    Create Wizard.java

---
+++ Spring/Spring Data/_04_HibernateCodeFirst/Gringotts/src/main/java/entities/Wizard.java
@@ -0,0 +1,26 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wizards")
+public class Wizard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", length = 60, nullable = false)
+    private String lastName;
+
+    @Column(length = 1000, columnDefinition = "TEXT")
+    private String note;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    
+}
