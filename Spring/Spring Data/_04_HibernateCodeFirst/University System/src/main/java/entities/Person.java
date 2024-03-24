472b285a61ae6cad5bdd2b34d8f8722a13bb16aa
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-03-25 00:08:17 +0200

    Create Person.java

---
+++ Spring/Spring Data/_04_HibernateCodeFirst/University System/src/main/java/entities/Person.java
@@ -0,0 +1,34 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
