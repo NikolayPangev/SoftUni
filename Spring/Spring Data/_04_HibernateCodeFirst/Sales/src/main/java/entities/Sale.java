9a66a17f5407b80c87bc41998918c0bc4fd33b4e
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-03-24 23:11:46 +0200

    Create Sale.java

---
+++ Spring/Spring Data/_04_HibernateCodeFirst/Sales/src/main/java/entities/Sale.java
@@ -0,0 +1,25 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Sale extends BaseEntity{
+    @ManyToOne
+    private Product product;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private StoreLocation storeLocation;
+
+    @Column
+    private Date date;
+}
