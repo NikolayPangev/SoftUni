e5692f385d5425f1c3fc75d1284a8d002dd3573b
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-03-25 22:06:09 +0200

    Create Diagnose.java

---
+++ Spring/Spring Data/_04_HibernateCodeFirst/Hospital/src/main/java/Diagnose.java
@@ -0,0 +1,25 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Diagnose {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String comments;
+
+    @OneToOne
+    @JoinColumn(name = "visitation_id")
+    private Visitation visitation;
+}
