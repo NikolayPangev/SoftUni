db695290008979dae6c32feba5e8ff07a6ecdfc2
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-03-25 22:06:45 +0200

    Create Prescription.java

---
+++ Spring/Spring Data/_04_HibernateCodeFirst/Hospital/src/main/java/Prescription.java
@@ -0,0 +1,22 @@
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table
+public class Prescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "visitation_id")
+    private Visitation visitation;
+}
