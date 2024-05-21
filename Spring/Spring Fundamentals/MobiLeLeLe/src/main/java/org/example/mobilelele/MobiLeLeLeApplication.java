69b52ef5f06bb7bfc091e59e9f6dd472c3f33811
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-05-21 13:41:57 +0300

    Create Project

--- Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/MobiLeLeLeApplication.java
+++ Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/MobiLeLeLeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobiLeLeLeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MobiLeLeLeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MobiLeLeLeApplication.class, args);
+	}
 
 }
