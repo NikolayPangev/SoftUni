0a756d308898e04c6e1638878436919491b085b9
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-06-27 20:15:29 +0300

    Created method to get all roles

--- Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/service/role/UserRoleService.java
+++ Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/service/role/UserRoleService.java
@@ -1,4 +1,11 @@
 package org.example.mobilelele.service.role;
 
+import org.example.mobilelele.model.dtoS.view.UserRoleViewDto;
+
+import java.util.List;
+
 public interface UserRoleService {
+
+    List<UserRoleViewDto> getAll();
+
 }
--- Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/service/role/UserRoleServiceImpl.java
+++ Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/service/role/UserRoleServiceImpl.java
@@ -1,27 +1,49 @@
 package org.example.mobilelele.service.role;
 
+import org.example.mobilelele.model.dtoS.view.UserRoleViewDto;
+import org.example.mobilelele.model.entities.UserRole;
+import org.example.mobilelele.model.enums.Role;
 import org.example.mobilelele.repository.RoleRepository;
 import org.example.mobilelele.service.init.DataBaseInitService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserRoleServiceImpl implements UserRoleService, DataBaseInitService {
 
     private final RoleRepository roleRepository;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public UserRoleServiceImpl(RoleRepository roleRepository) {
+    public UserRoleServiceImpl(RoleRepository roleRepository, ModelMapper modelMapper) {
         this.roleRepository = roleRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public void dbInit() {
+        List<UserRole> roles = new ArrayList<>();
 
+        roles.add(new UserRole().setRole(Role.USER));
+        roles.add(new UserRole().setRole(Role.ADMIN));
+
+        this.roleRepository.saveAllAndFlush(roles);
     }
 
     @Override
     public boolean isDbInit() {
         return this.roleRepository.count() > 0;
+    }
+
+    public List<UserRoleViewDto> getAll() {
+        return this.roleRepository.findAll()
+                .stream()
+                .map(role -> this.modelMapper.map(role, UserRoleViewDto.class))
+                .collect(Collectors.toList());
     }
 }
