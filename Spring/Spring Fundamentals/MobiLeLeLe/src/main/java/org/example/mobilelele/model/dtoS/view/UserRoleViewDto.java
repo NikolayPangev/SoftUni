dd1dbd3f93def0d4b3e74e2a4416fbca6e99fd7f
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2024-06-27 20:15:52 +0300

    Created UserRoleViewDto

---
+++ Spring/Spring Fundamentals/MobiLeLeLe/src/main/java/org/example/mobilelele/model/dtoS/view/UserRoleViewDto.java
@@ -0,0 +1,17 @@
+package org.example.mobilelele.model.dtoS.view;
+
+import org.example.mobilelele.model.enums.Role;
+
+public class UserRoleViewDto {
+
+    private Role role;
+
+    public Role getRole() {
+        return role;
+    }
+
+    public UserRoleViewDto setRole(Role role) {
+        this.role = role;
+        return this;
+    }
+}
